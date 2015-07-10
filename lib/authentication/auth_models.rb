ffff2930355ac5d6c1ecc0e9a721d2f797a54d68
Colin Walker <cjwalker@sfu.ca>
2015-07-09 22:28:57 -0700

    create login models and methods

---
+++ lib/authentication/auth_models.rb
@@ -0,0 +1,24 @@
+module AuthModels
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  before_validation :default_role
+  validate  :correct_role
+
+  # sends a token through the bcrypt system to see if a user is authenticated
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  # Returns the hash digest of the given string.
+  def Admin.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
