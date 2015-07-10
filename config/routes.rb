9bfe09e5081129f88024cac74145d969b171e322
Colin Walker <cjwalker@sfu.ca>
2015-07-09 22:42:21 -0700

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'login' => 'sessions#new', as: 'login_path'
   resources :organizations
   resources :users
   namespace :admin do
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
     resources :users
     resources :supplies
     resources :admins
+    resources :sessions
     get '/admin' => 'dashboard#index', as: 'admin_root_path'
   end
   root to: 'home#index'
