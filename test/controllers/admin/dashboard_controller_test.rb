efc6c568520864bb7dbd2de6e8fb4566911b5d19
Colin Walker <cjwalker@sfu.ca>
2015-07-28 08:21:46 -0700

    set up tests

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class Admin::DashboardControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+
 
 end
