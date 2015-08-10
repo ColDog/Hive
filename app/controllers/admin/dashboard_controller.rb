376b5c8a7c7352b4c5ed49a4a8bd6cc130a74d13
Colin Walker <cjwalker@sfu.ca>
2015-08-10 12:21:24 -0700

    total and in use for report

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,7 +6,9 @@ class Admin::DashboardController < ApplicationController
 
   def report
     @search = params[:search]
-    @list = SupplyList.all.search(@search)
+    list = SupplyList.all.search(@search)
+    @in_use = list.where('organization_id IS NOT NULL OR user_id IS NOT NULL').count
+    @total = list.count
     respond_to { |format| format.html { render 'report', layout: false } }
   end
 
