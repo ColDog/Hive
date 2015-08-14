ad13da159b651195765b5e1cc447cede4f411e0f
Colin Walker <cjwalker@sfu.ca>
2015-08-13 20:00:30 -0700

    fix user form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807061652) do
+ActiveRecord::Schema.define(version: 20150814025819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 20150807061652) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "linked_in"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
