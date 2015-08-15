b81a720744ba25a8034695f2f919a9e1eea77d40
Colin Walker <cjwalker@sfu.ca>
2015-08-15 08:29:15 -0700

    create new uploader

---
+++ app/uploaders/agreement_uploader.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+class ServiceAgreementUploader < CarrierWave::Uploader::Base
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  def store_dir
+    'agreements/'
+  end
+
+  def filename
+    @name ||= "SA#{@filename.split('.')[0]}#{timestamp}.#{file.extension}" if original_filename.present?
+  end
+
+  def timestamp
+    var = :"@#{mounted_as}_timestamp"
+    model.instance_variable_get(var) or model.instance_variable_set(var, Time.now.strftime('%C-%m-%dT%H%M%S'))
+  end
+
+
+end
