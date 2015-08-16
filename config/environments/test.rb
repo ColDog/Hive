6f40bf6981250a5955bc5b0da0c29bd9e5422707
Colin Walker <cjwalker@sfu.ca>
2015-08-16 12:16:19 -0700

    added codeclimate variable

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,7 @@
+ENV['CODECLIMATE_REPO_TOKEN'] = 'fe650dcb995b461297007c1131b7ed8dd91c63e6a4158c5c59f7ffe110d6d597'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
