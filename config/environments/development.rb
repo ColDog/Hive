24f360f7c890a56c6ada9955180a51253fc38f2f
Colin Walker <cjwalker@sfu.ca>
2015-08-17 18:18:15 -0700

    add dalli to test memcache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,12 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # caching settings
+  config.action_controller.perform_caching  = true
+  config.cache_store                        = :mem_cache_store
 
   # Mailer settings
   config.action_mailer.raise_delivery_errors = false
