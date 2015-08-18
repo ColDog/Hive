5191ea876e6da68035cc6d326d39087eccfa63a7
Colin Walker <cjwalker@sfu.ca>
2015-08-18 07:39:26 -0700

    forgot glob path

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,7 +4,7 @@ class UserMailerTest < ActionMailer::TestCase
 
   test 'content only' do
     pars = {
-      'content' => 'hello there person!', 'sign_in_link' => '0',
+      'content' => 'hello there person!', 'sign_in_link' => '0', 'subject' => 'A users subject',
       'supplies' => '0', 'account_type' => '0', 'attachments' => []
     }
 
@@ -12,9 +12,9 @@ class UserMailerTest < ActionMailer::TestCase
     tok = 'p34zeA5xA7OsANGV8KDrKA'
     mail = UserMailer.new_user( user, tok,  pars )
 
-    assert_equal 'New Account Created at Hive', mail.subject
-    assert_equal ['cjwalker@sfu.ca'],           mail.to
-    assert_equal ['info@hivevancouver.com'],    mail.from
+    assert_equal 'A users subject',           mail.subject
+    assert_equal ['cjwalker@sfu.ca'],         mail.to
+    assert_equal ['info@hivevancouver.com'],  mail.from
 
     matches = [
       'mailto:info@hivevancouver.com', '<p>hello there person!</p>',  'Hello Colin Walker,'
@@ -34,7 +34,7 @@ class UserMailerTest < ActionMailer::TestCase
 
   test 'everything' do
     pars = {
-      'content' => 'hello there person!', 'sign_in_link' => '1',
+      'content' => 'hello there person!', 'sign_in_link' => '1', 'subject' => 'New Account Created at Hive',
       'supplies' => '1', 'account_type' => '1', 'attachments' => [1,2]
     }
 
