16a3a1fa691503d6355193c4bcb0723a8300a41f
Colin Walker <cjwalker@sfu.ca>
2015-08-18 12:07:17 -0700

    change buckets and region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region:                'us-west-1'
+      region:                'us-west-2'
     }
-    config.fog_directory =   'hive-colin'
+    config.fog_directory =   'hive.members'
   end
 end
