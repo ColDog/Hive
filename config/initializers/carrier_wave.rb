17d16437d37d005cc95537854c122e19f9ff8596
Colin Walker <cjwalker@sfu.ca>
2015-08-19 07:47:57 -0700

    update carierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,19 +1,25 @@
-CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV['S3_BUCKET']
-  config.aws_acl    = 'authenticated-read'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage    = :aws
+    config.aws_bucket = ENV['S3_BUCKET']
+    config.aws_acl    = 'authenticated-read'
 
-  config.aws_attributes = {
-    expires: 1.week.from_now.httpdate,
-    cache_control: 'max-age=604800'
-  }
+    config.aws_attributes = {
+      expires: 1.week.from_now.httpdate,
+      cache_control: 'max-age=604800'
+    }
 
-  config.aws_credentials = {
-    access_key_id:     ENV['S3_ACCESS_KEY'],
-    secret_access_key: ENV['S3_SECRET_KEY']
-  }
+    config.aws_credentials = {
+      access_key_id:     ENV['S3_ACCESS_KEY'],
+      secret_access_key: ENV['S3_SECRET_KEY']
+    }
 
-  config.aws_authenticated_url_expiration = 3600
+    config.aws_authenticated_url_expiration = 3600
 
-  @aws_signer = -> (unsigned_url, options) { Aws::CF::Signer.sign_url unsigned_url, options }
+    @aws_signer = -> (unsigned_url, options) { Aws::CF::Signer.sign_url unsigned_url, options }
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
 end
